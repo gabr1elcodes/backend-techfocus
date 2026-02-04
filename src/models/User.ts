d5fd68bd7a3453d90cbb090867befd99c2d35344
Gabriel Oliveira <gabrieloliveira30p@gmail.com>
2026-02-04 16:49:11 -0300

    feat: pequeno 'bug'

--- src/models/User.ts
+++ src/models/User.ts
@@ -9,7 +9,7 @@ export interface IUser extends Document {
 }
 
 const userSchema = new Schema<IUser>({
-  name: { type: String, required: true },
+  name: { type: String, required: false },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
 });
